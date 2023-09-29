f3c63eafdf2c57d09f14a8325c1d2841970b1b4b
tejas.krishnan81 <tejas.krishnan81@gmail.com>
2023-09-29 12:09:02 -0400

    final check"

--- server/app.py
+++ server/app.py
@@ -878,8 +878,8 @@ def create_calendar_file():
         mimetype='text/calendar'
     )
 
-# if __name__=="__main__":
-#     app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 # Text detection using EAST: finds bounding boxes on all words but not accurate enough
 # =============================================================================
